536403b62c5ec7daf12ec1df75082f8323ea35da
Corey Adolphus <83255706+cadolphus@users.noreply.github.com>
2024-12-03 14:24:17 +1100

    Update variables.tf

--- gcp/01-bootstrap/variables.tf
+++ gcp/01-bootstrap/variables.tf
@@ -1,3 +1,5 @@
+# This file just declares the names, types, and default values for the variables used in the main.tf script.
+# To set the values of these variables, do that in the "terraform.tfvars" file in the same directory.
 
 # GCP parameters
 variable "gcp_project_id" {
@@ -11,4 +13,22 @@ variable "gcp_region" {
 variable "gcp_zone" {
   type = string
   description = "Enter the default GCP zone to use for this script - Eg: australia-southeast1-b"
+}
+variable "gcs_tf_state_bucket" {
+  type = string
+  description = "Enter the default GCP zone to use for this script - Eg: australia-southeast1-b"
+}
+variable "vpc_name" {
+  type = string
+  description = "Enter the name to use when creating the VPC Network for this bootstrap - Eg: vpc-dev"
+}
+variable "vpc_routing_mode" {
+  type = string
+  description = "Enter the routing mode to use for this VPC - either 'GLOBAL' or 'REGIONAL'"
+  default = "GLOBAL"
+}
+variable "vpc_mtu" {
+  type = number
+  description = "Enter the MTU to use for this VPC - between 1460 and 1500 inclusive. If set to 0 (meaning MTU is unset), the network will default to 1460 automatically."
+  default = 0
 }
