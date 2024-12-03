322ecbc5bc1485640d63cb469b1e9dca6d95b6c8
Corey Adolphus <83255706+cadolphus@users.noreply.github.com>
2024-12-03 14:23:49 +1100

    Update main.tf

--- gcp/01-bootstrap/main.tf
+++ gcp/01-bootstrap/main.tf
@@ -50,3 +50,23 @@ locals {
 output "rand_prefix_hex" {
   value = local.prefix
 }
+
+#########
+## VPC ##
+#########
+
+# Create an empty VPC network (without any subnets, routes, or firewall rules)
+resource "google_compute_network" "vpc" {
+  name                    = "tf${local.prefix}-${var.vpc_name}"
+  auto_create_subnetworks = false
+  routing_mode            = var.vpc_routing_mode
+  mtu                     = var.vpc_mtu
+}
+# Create output variables of the VPC so it can be referenced/used by other modules
+output "vpc_name" {
+  value = google_compute_network.vpc.name
+}
+output "vpc_id" {
+  value = google_compute_network.vpc.id
+}
+
